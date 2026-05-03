49a5674347315db090cdbc709b003c1627c7c53d
Tenakin <33879951+TenakinVFX@users.noreply.github.com>
2026-05-03 04:36:38 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -56,9 +56,7 @@ def get_info():
             })
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    finally:
-        if cookie_file and os.path.exists(cookie_file):
-            os.unlink(cookie_file)
+
 
 
 @app.route('/api/download', methods=['POST'])
